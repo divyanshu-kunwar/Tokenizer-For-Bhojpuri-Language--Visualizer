592cc4c2a1f8791370535c33b6df512f2599431e
Divyanshu Kunwar <68821907+divyanshu-kunwar@users.noreply.github.com>
2024-08-12 02:46:52 +0530

    config changes for deployment
    config changes
    
    removed local ip
    
    corrected fav icon path
    
    just trying config
    
    another config
    
    removed local cors thing

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
 from fastapi import FastAPI , Request
-from starlette.middleware.sessions import SessionMiddleware
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 from minbpe import RegexTokenizerFast
 import regex as re
-import uvicorn
 
-app = FastAPI(debug=True)
-app.add_middleware(SessionMiddleware, secret_key="SECRET101")
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins="*",
-    allow_credentials=True,  # Whether to allow credentials (cookies, authorization headers, etc.)
-    allow_methods=["*"], 
-    allow_headers=["*"],
-)
+app = FastAPI()
 
 app.mount("/assets", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="Views")
@@ -49,4 +37,5 @@ async def tokenize(text: str, tokenizer_name: str = "BH10K"):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+        import uvicorn
+        uvicorn.run(app)
